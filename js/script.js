ebfe5c9022c28a36e9e9e0170f03bc547d44177c
chaima garoui <chaimagaroui2@gmail.com>
2024-04-05 17:00:58 +0200

    debutt fonction supprimer

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const addBtn = document.querySelector('#btn');
 addBtn.addEventListener('click',addTask);
 
-// //creation de la fonction ajout :
+//creation de la fonction ajout :
 
 const taskcard = document.querySelector('.todoCards');
 const tasksContainer = document.querySelector("#todoCards");
@@ -16,6 +16,38 @@ function addTask(){
     tasksContainer.appendChild(newTask)                     //on ajoute cette nouvelle card dans le DOM ( à la fin de la liste des enfants de l'élément tasksContainer)
     tasksContainer.appendChild(newTask);
     
+}
+
+//On ajoute un écouteur d’évènement sur le bouton pour appeler une fonction suppression:
+const delBtn =document.querySelector('.delBtn');
+delBtn.addEventListener('click',function(){
+    deleteTask(taskcard);
+});
+
+//on crée la fonctuiion supprimer 
+
+function deleteTask(task){
+    task.remove();
+}
+
+
+
+
+
+
+
+//On ajoute l’écouteur d’évènement sur l’élément cloné dans la fonction ajout, afin de faire fonctionner le bouton suppression sur les nouvelles cards
+function addTask(){
+    const newTask = taskcard.cloneNode(true)
+    const newDelBtn = newTask.querySelector('.delBtn')
+    const newTextArea = newTask.querySelector('.task')
+    newTextArea.value = "new Task"
+    newDelBtn.addEventListener('click',function(){
+        deleteTask(newTask);
+});
+    tasksContainer.appendChild(newTask)
+    updateCount();
+
 }
 
 
